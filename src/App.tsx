3c73638694660fc112fc48fa7b37d16a9a8d7ed8
Elliot Nash <elliotnash@gmail.com>
2025-03-12 09:23:58 -0700

    Animate position

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,15 @@ import { useSpring, animated } from "@react-spring/three";
 import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
 
 const AnimatedGarageDoor = ({ isOpen }: { isOpen: boolean }) => {
-  const { rotation } = useSpring({
+  const { rotation, position } = useSpring({
     rotation: isOpen ? [-Math.PI / 2, 0, 0] : [0, 0, 0],
-    config: { mass: 1, tension: 180, friction: 20 },
+    position: isOpen ? [0, 1, -1] : [0, 0, 0],
+    config: { mass: 1, tension: 25, friction: 8 },
   });
 
   return (
     // biome-ignore lint/suspicious/noExplicitAny: Vector3
-    <animated.mesh rotation={rotation as any} position={[0, 0, 0]}>
+    <animated.mesh rotation={rotation as any} position={position as any}>
       <boxGeometry args={[2, 2, 0.1]} />
       <meshStandardMaterial color="gray" />
       <mesh position={[0.8, 0, 0.06]}>
@@ -31,7 +32,7 @@ const Scene = ({ isOpen }: { isOpen: boolean }) => {
       <pointLight position={[10, 5, 5]} intensity={1000} castShadow />
 
       {/* Garage structure */}
-      <mesh position={[0, -1, 0]}>
+      <mesh position={[0, -1.1, 0]}>
         <boxGeometry args={[2.5, 0.2, 2]} />
         {/* Increase material roughness for better light response */}
         <meshStandardMaterial color="#555" roughness={0.4} metalness={0.2} />
@@ -46,8 +47,8 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <View className="flex-1">
-      <Canvas className="flex-1">
+    <View className="flex-1 bg-black">
+      <Canvas camera={{ position: [1, 2, 8], fov: 50 }} className="flex-1">
         <Scene isOpen={isOpen} />
       </Canvas>
 
