cb63656232aec5236a015c6b8a7c491daaf04f41
Elliot Nash <elliotnash@gmail.com>
2025-03-12 14:41:03 -0700

    Load door obj

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,12 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname);
+const config = {
+  ...getDefaultConfig(__dirname),
+  resolver: {
+    sourceExts: ["js", "jsx", "json", "ts", "tsx", "cjs", "mjs"],
+    assetExts: ["glb", "gltf", "png", "jpg", "obj"],
+  },
+};
 
 module.exports = withNativeWind(config, { input: "./src/global.css" });
--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,40 @@ import "./global.css";
 import { useState } from "react";
 import { Canvas } from "@react-three/fiber/native";
 import { useSpring, animated } from "@react-spring/three";
-import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
+import { TouchableOpacity, Text, View } from "react-native";
+import { useLoader } from "@react-three/fiber";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
 const AnimatedGarageDoor = ({ isOpen }: { isOpen: boolean }) => {
+  const panel = useLoader(OBJLoader, "/assets/single_panel.obj");
+
   const { rotation, position } = useSpring({
     rotation: isOpen ? [-Math.PI / 2, 0, 0] : [0, 0, 0],
     position: isOpen ? [0, 1, -1] : [0, 0, 0],
-    config: { mass: 1, tension: 25, friction: 8 },
+    config: { mass: 1, tension: 100, friction: 16 },
+  });
+
+  panel.traverse((child) => {
+    //@ts-ignore
+    if (child.isMesh) {
+      //@ts-ignore
+      child.material.color.set("grey");
+    }
   });
 
   return (
     // biome-ignore lint/suspicious/noExplicitAny: Vector3
     <animated.mesh rotation={rotation as any} position={position as any}>
-      <boxGeometry args={[2, 2, 0.1]} />
-      <meshStandardMaterial color="gray" />
-      <mesh position={[0.8, 0, 0.06]}>
+      {/* <boxGeometry args={[2, 2, 0.1]} /> */}
+      <primitive object={panel.clone()} scale={0.5} position={[0, -1, 0]} />
+      <primitive object={panel.clone()} scale={0.5} position={[0, -0.5, 0]} />
+      <primitive object={panel.clone()} scale={0.5} position={[0, 0, 0]} />
+      <primitive object={panel.clone()} scale={0.5} position={[0, 0.5, 0]} />
+      <meshStandardMaterial color="grey" />
+      {/* <mesh position={[0.8, 0, 0.06]}>
         <cylinderGeometry args={[0.03, 0.03, 0.2]} />
         <meshStandardMaterial color="darkgray" />
-      </mesh>
+      </mesh> */}
     </animated.mesh>
   );
 };
@@ -50,6 +66,7 @@ export default function App() {
     <View className="flex-1 bg-black">
       <Canvas camera={{ position: [1, 2, 8], fov: 50 }} className="flex-1">
         <Scene isOpen={isOpen} />
+        {/* <Scene2 /> */}
       </Canvas>
 
       <TouchableOpacity
